bcca190107ff20d0cca6ddb0e86429b9863bacc4
Daniel M <danielmiao2019@icloud.com>
2021-08-25 11:05:29 -0700

    added operator click events

--- js/calculator.js
+++ js/calculator.js
@@ -9,6 +9,16 @@ for (x of document.getElementsByClassName("number")) {
 	});
 };
 
+for (x of document.getElementsByClassName("operator")) {
+	x.addEventListener("click", function() {
+		if (!["÷", "×", "–", "+", "."].includes(document.getElementById("result").value.slice(-1))) {
+			document.getElementById("result").value += this.innerHTML;
+                };
+		$("#result").scrollTop($("#result")[0].scrollHeight);
+	});
+};
+
 document.getElementById("=").addEventListener("click", function() {
-	document.getElementById("result").value = eval(document.getElementById("result").value)
+	document.getElementById("result").value = eval(document.getElementById("result").value.replace("–", "-").replace("×", "*").replace("÷", "/"));
 });
+
