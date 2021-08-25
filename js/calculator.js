8801c94f019a6b753a508b8b2d78bfdd2de3191f
Daniel M <danielmiao2019@icloud.com>
2021-08-25 09:49:45 -0700

    update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,33 +1,13 @@
-// Roy Yi
+// Daniel M
 // Web calculator application
-// 5/27/21
+// 8/25/21
 
-var res = document.getElementById("res");
+for (x of document.getElementsByClassName("number")) {
+	x.addEventListener("click", function() {
+		document.getElementById("result").value += this.innerHTML;
+	});
+};
 
-// append character for each operator or operand, using event delegation
-const parent = document.getElementById("btns");
-
-parent.addEventListener("click", event => {
-    if(event.target.classList.contains("operand") ||
-       event.target.classList.contains("operator")) {
-        res.innerHTML += event.target.innerHTML;
-    }
+document.getElementById("=").addEventListener("click", function() {
+	document.getElementById("result").value = eval(document.getElementById("result").value)
 });
-
-// event listeners for clear and equal buttons
-var clear_btn = document.getElementById("clr_btn");
-clear_btn.addEventListener("click", function() {
-    res.innerHTML = "";
-})
-
-var eval_btn = document.getElementById("eql_btn");
-eval_btn.addEventListener("click", function() {
-    res.innerHTML = Math.floor(eval(res.innerHTML));
-})
-
-
-// old way: add event listener for each element of class
-// var values = ["btn0", "btn1", "btnSum","btnSub", "btnMul", "btnDiv"];
-// for (let i in values) {
-//     document.getElementById(values[i]).addEventListener("click", append_char);
-// }
